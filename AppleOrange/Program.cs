ec1356ded36e635edd9fc521ac134dec4ab5ee6f
genaptashic <genaptashic@gmail.com>
2020-12-03 15:40:20 +0300

    Add from Windows

---
+++ AppleOrange/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace AppleOrange
+{
+    class Program
+    {
+        // Complete the countApplesAndOranges function below.
+        static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges)
+        {
+            int appleCount = 0;
+            int orangeCount = 0;
+            int i = 0;
+
+            for ( i = 0; i < apples.Length; i++)
+            {
+                
+                if ((apples[i]+a) >= s && (apples[i] + a) <= t)
+                {
+                    appleCount++;
+                }
+            }
+            for (i = 0; i < oranges.Length; i++)
+            {
+                
+                if ((oranges[i]+b) >= s && (oranges[i] + b) <= t)
+                {
+                    orangeCount++;
+                }
+            }
+            Console.WriteLine(appleCount);
+            Console.WriteLine(orangeCount);
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            string[] st = Console.ReadLine().Split(' ');
+
+            int s = Convert.ToInt32(st[0]);
+
+            int t = Convert.ToInt32(st[1]);
+
+            string[] ab = Console.ReadLine().Split(' ');
+
+            int a = Convert.ToInt32(ab[0]);
+
+            int b = Convert.ToInt32(ab[1]);
+
+            string[] mn = Console.ReadLine().Split(' ');
+
+            int m = Convert.ToInt32(mn[0]);
+
+            int n = Convert.ToInt32(mn[1]);
+
+            int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+            ;
+
+            int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+            ;
+            countApplesAndOranges(s, t, a, b, apples, oranges);
+        
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        class Result
+        {
+
+            public static List<int> gradingStudents(int gradesCount, List<int> grades)
+            {
+                for (int i = 0; i < gradesCount; i++)
+                {
+                    if (grades[i] > 37)
+                    {
+                        if ((grades[i] / 5 + 1) * 5 - grades[i] < 3)
+                        {
+                            grades[i] = (grades[i] / 5 + 1) * 5;
+                        }
+                    }
+
+                }
+                return grades;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = Result.gradingStudents(gradesCount, grades);
+            Console.WriteLine(Convert.ToString(result));
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
